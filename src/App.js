71b92fc464888ab3deb4a7c15d2abe074d6774e9
yaojun <yaoj@bsfit.com.cn>
2019-10-30 10:10:31 +0800

    import ant-design framework

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Upload, Icon, message } from 'antd';
 
 function App() {
+  const { Dragger } = Upload;
+
+  const props = {
+    name: 'file',
+    multiple: true,
+    action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
+    onChange(info) {
+      const { status } = info.file;
+      if (status !== 'uploading') {
+        console.log(info.file, info.fileList);
+      }
+      if (status === 'done') {
+        message.success(`${info.file.name} file uploaded successfully.`);
+      } else if (status === 'error') {
+        message.error(`${info.file.name} file upload failed.`);
+      }
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className='App'>
+      
+      <Dragger {...props}>
+        <p className='ant-upload-drag-icon'>
+          <Icon type='inbox' />
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p className='ant-upload-text'>点击或拖拽文件到这个区域进行文件上传</p>
+        <p className='ant-upload-hint'>支持多文件上传</p>
+      </Dragger>
     </div>
   );
 }
